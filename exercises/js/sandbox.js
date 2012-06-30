09ceb9bc79ca7cff2eea019eefc9de4e371171fe
Apoorv Saxena <apoorv.saxena@vinsol.com>
2012-06-30 18:39:37 +0530

    jQuery Exercise 3

--- exercises/js/sandbox.js
+++ exercises/js/sandbox.js
@@ -1,17 +1,39 @@
 $(document).ready(function(){
-	console.log($('div .module'));
+	function selecting_exercise(){
+		console.log($('div .module'));
 
-	console.log($('#myList li:nth-child(3)'));
-	console.log($('#myList li:eq(2)'));
-	console.log($('#myList li')[2]);
-	
-	console.log($('label[for="q"]'));
-	
-	console.log($('*:hidden'));
-	
-	console.log($('img[alt]'));
+		console.log($('#myList li:nth-child(3)'));
+		console.log($('#myList li:eq(2)'));
+		console.log($('#myList li')[2]);
+		
+		console.log($('label[for="q"]'));
+		
+		console.log($('*:hidden'));
+		
+		console.log($('img[alt]'));
 
-	console.log($('#myList li:nth-child(odd)'));
+		console.log($('#myList li:nth-child(odd)'));
 
-	console.log($('tbody tr:nth-child(odd)'));
+		console.log($('tbody tr:nth-child(odd)'));		
+	}
+
+	function traversing_exercise(){
+		$('img[alt]').each(function(){
+			console.log(this.alt);
+		});
+
+		console.log($('input[name="q"]').parent().addClass('myclass'));
+
+		console.log($('#myList[class="current"]').removeClass());
+
+		console.log($('#myList li[class*="current"]').removeClass().next().addClass('current'));
+
+		$('#specials select').parentsUntil('form').each(function(){
+			console.log($(this).find('input[type="submit"]'));
+		});
+
+		$('#slideshow li:first').addClass('current').siblings().addClass('disabled');		
+	}
+	traversing_exercise();
+
 });
